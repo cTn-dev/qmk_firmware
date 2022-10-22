23ef470e4dbb9066ea72ba6ef094bcd150261a6a
Stefan <ctndesigner@gmail.com>
2022-10-22 12:47:56 +0200

    switch debounce type to sym_eager_pk

--- keyboards/keychron/q3/ansi/rules.mk
+++ keyboards/keychron/q3/ansi/rules.mk
@@ -22,6 +22,7 @@ RGB_MATRIX_DRIVER = CKLED2001
 LTO_ENABLE = yes
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = embedded_flash
+DEBOUNCE_TYPE = sym_eager_pk
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
